5037e715ecee79676c75d01ef5e5d30063a2ea24
den <solovyov.dd@gmail.com>
2024-08-09 13:07:12 +0200

    Fix bug

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -19,7 +19,7 @@ const Provider = ({children}: {children: ReactNode}) => {
             resolveMentionSuggestions={async ({ text, roomId }) => {
                 const roomUsers = await getDocumentUsers({
                     roomId,
-                    currentUser: clerkUser?.emailAddresses[0].emailAddresses!,
+                    currentUser: clerkUser?.emailAddresses[0].emailAddress!,
                     text
                 })
                 return roomUsers
